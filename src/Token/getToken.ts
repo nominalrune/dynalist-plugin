509e87ce15e21102246cb56636bee199279d7aba
yoshinori yokoyama <nominalrune.yokoyama@gmail.com>
2024-08-03 21:16:29 +0900

    fix infinit roop

--- src/Token/getToken.ts
+++ src/Token/getToken.ts
@@ -5,8 +5,6 @@ export default async function getToken(secretStorage: SecretStorage) {
 	const token = await secretStorage.get(TOKEN_KEY);
 	if (!token) {
 		window.showErrorMessage('API Token not set. Please save your token first.');
-		const result = await commands.executeCommand('dynalist-plugin.update-token');
-		return await getToken(secretStorage);
 	}
 	return token;
 }
